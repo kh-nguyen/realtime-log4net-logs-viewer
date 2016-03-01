98b7c83d6fd46588d908183e841ff74eb4c918ee
Khoa Nguyen <khoa321a@gmail.com>
2016-03-01 15:24:26 -0800

    add timeago

--- realtime-log4net-logs-viewer/realtime-log4net-logs-viewer/App_Start/Startup.Auth.cs
+++ realtime-log4net-logs-viewer/realtime-log4net-logs-viewer/App_Start/Startup.Auth.cs
@@ -1,5 +1,4 @@
 ﻿using Owin;
-using realtime_log4net_logs_viewer.Models;
 
 namespace realtime_log4net_logs_viewer
 {
@@ -8,8 +7,7 @@ namespace realtime_log4net_logs_viewer
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+
         }
     }
 }
